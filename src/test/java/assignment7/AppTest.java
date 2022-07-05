ca7e12f35c7e1f5e3b8400dbc6ec8893150ebac4
GageVanK <gagevank@gmail.com>
2022-07-05 11:11:22 -0400

    Added Main + Test Classes

---
+++ src/test/java/assignment7/AppTest.java
@@ -0,0 +1,25 @@
+package assignment7;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void FibEquals() {
+		int val = 10;
+		
+		assertEquals(55, App.fibMethod(val));
+		
+	}
+	
+	@Test
+	public void FibEqualsFail() {
+		int val = 10;
+		
+		assertEquals(10, App.fibMethod(val));
+		
+	}
+
+}
